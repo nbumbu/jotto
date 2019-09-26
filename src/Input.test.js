baf2d12113b80317ed4ad8d4a34adc8d467ed053
Nick <sahas023@gmail.com>
2019-09-27 01:18:07 +0200

    added input component and test for rendering

--- src/Input.test.js
+++ src/Input.test.js
@@ -31,18 +31,25 @@ describe('render', () => {
     });
 
     describe('word has been guessed', () => {
-
+        let wrapper;
+        beforeEach(() => {
+            const initialState = {success: false};
+            wrapper = setup(initialState);
+        })
 
         test('renders the component without any error', () => {
-
+            const component = findByTestAttr(wrapper, 'component-input');
+            expect(component.length).toBe(1);
         });
 
         test('does not render the input box', () => {
-
+            const inputBox = findByTestAttr(wrapper, 'input-box');
+            expect(inputBox.length).toBe(1);
         });
 
         test('does not render the submit button', () => {
-
+            const submitButton = findByTestAttr(wrapper, 'submit-button');
+            expect(submitButton.length).toBe(1);
         });
 
     });
