a50d51af46352ec741b530eb9a6184b55183e6ff
Nick <sahas023@gmail.com>
2019-09-27 11:27:34 +0200

    added thunk for guessWords piece of state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import {combineReducers} from "redux";
 import success from './successReducer';
 import guessedWords from "./guessedWordsReducers";
+import secretWord from "./secretWord.reducer";
+
 
 export default combineReducers({
     success,
-    guessedWords
+    guessedWords,
+    secretWord
 });
